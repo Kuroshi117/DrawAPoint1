c4327de422c5915f74151518213bd4f5e19ca839
Kuroshi117 <Phoebe.Huang@iam.local>
2018-09-13 15:17:19 -0500

    Add project files.

---
+++ DrawAPoint1/HelloPoint1.js
@@ -0,0 +1,27 @@
+﻿var VSHADER_SOURCE =
+    'void main() {\n' +
+    '  gl_Position = vec4(0.0, 0.0, 0.0, 1.0);\n' + // Set the vertex coordinates of the point
+    '  gl_PointSize = 10.0;\n' +                    // Set the point size
+    '}\n';
+var FSHADER_SOURCE =
+    'void main() {\n' +
+    '	gl_FragColor= vec4(1.0,0.0,0.0,1.0);\n' +
+    '}\n';
+
+function main() {
+    var canvas = document.getElementById('webgl');
+    var gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to initialize shaders.');
+        return;
+    }
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.POINTS, 0, 1);
+}
